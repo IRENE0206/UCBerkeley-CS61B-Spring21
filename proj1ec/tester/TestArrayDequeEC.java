30fda25beb58e533198552cab38c5ff9d912f949
Irene Jiaxin Fan <irene0206@outlook.com>
2022-06-02 09:47:13 +0800

    Rewrote the string message provided to assertEquals

--- proj1ec/tester/TestArrayDequeEC.java
+++ proj1ec/tester/TestArrayDequeEC.java
@@ -11,24 +11,29 @@ public class TestArrayDequeEC {
     public void testStudent() {
         StudentArrayDeque<Integer> sad = new StudentArrayDeque<>();
         ArrayDequeSolution<Integer> ads = new ArrayDequeSolution<>();
+        String message = "";
         for (int i = 0; i < 10000; i++) {
             int operationNumber = StdRandom.uniform(0, 4);
             if (operationNumber == 0) {
                 sad.addFirst(i);
                 ads.addFirst(i);
-                assertEquals("addFirst(" + i + ")\naddFirst(" + i +")", ads.size(), sad.size());
+                message = message + "\naddFirst(" + i + ")";
+                assertEquals(message, ads.size(), sad.size());
             } else if (operationNumber == 1) {
                 sad.addLast(i);
                 ads.addLast(i);
-                assertEquals("addLast(" + i + "\naddLast(" + i +")", ads.size(), sad.size());
+                message = message + "\naddLast(" + i + ")";
+                assertEquals(message, ads.size(), sad.size());
             } else if (operationNumber == 2 && sad.size() > 0 && ads.size() > 0) {
                 Integer sadFirst = sad.removeFirst();
                 Integer adsFirst = ads.removeFirst();
-                assertEquals("removeFirst()\nremoveFirst()", adsFirst, sadFirst);
+                message = message + "\nremoveFirst()";
+                assertEquals(message, adsFirst, sadFirst);
             } else if (operationNumber == 3 && sad.size() > 0 && ads.size() > 0) {
                 Integer sadLast = sad.removeLast();
                 Integer adsLast = ads.removeLast();
-                assertEquals("removeLast()\nremoveLast()", adsLast, sadLast);
+                message = message + "\nremoveLast()";
+                assertEquals(message, adsLast, sadLast);
             }
         }
     }
